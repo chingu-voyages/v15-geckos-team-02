71eea654f4c24a7e56b3b1b8e83c8c0f9e6e1179
kfarfan <34223564+kfarfan@users.noreply.github.com>
2020-01-14 23:13:31 -0500

    Created Card.css
    Added Card.css for styling, removed inline styles.

--- mixed_drinks/src/components/Card.js
+++ mixed_drinks/src/components/Card.js
@@ -1,33 +1,16 @@
 import React from 'react';
+import './Card.css';
 
 const Card = props => {
-    let divStyle = {
-        backgroundcolor: 'rgba(255, 255, 255, 0.9)',
-        border: '1px solid #c5c5c5',
-        borderRadius: '3px',
-        width: '280px',
-        height: '300px',
-        float: 'left',
-        margin: '10px',
-        boxShadow: '1px 2px 2px #eeee',
-    }
-    let ulStyle = {
-        padding: "0px",
-        marginRight: '50px'
-    }
-
-    let liStyle = {
-        textAlign: "center"
-    }
     return (
-        <div key={props.id} style={divStyle}>
+        <div className="div" key={props.id}>
             <h3>{props.drinkName}</h3>
             <p>{props.instructions}</p>
-            <ul style={ulStyle}>
+            <ul className="ul">
                 {props.ingredients.map((ingredient, index) => {
                     return props.measurements[index] === null ? 
-                    <li style={liStyle}>{ingredient}</li> : 
-                    <li style={liStyle}>{`${ingredient} - ${props.measurements[index]}`}</li>
+                    <li className="li">{ingredient}</li> : 
+                    <li className="li">{`${ingredient} - ${props.measurements[index]}`}</li>
                 })}
             </ul>
         </div>
