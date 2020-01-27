e41e9535af1a548834407027dba02e05b1c198e7
kfarfan <34223564+kfarfan@users.noreply.github.com>
2020-01-26 22:45:18 -0500

    Added Constants.js and made edits to Card.js
    Added constatns.js
    Added url paramters to constants.js
    Removed urlPathmodifier from App.js
    -replaced fetch functions with appropriate constants
    -fixed Card.js indexing error for previous and next button

--- mixed_drinks/src/components/Card.js
+++ mixed_drinks/src/components/Card.js
@@ -5,13 +5,8 @@ import AddDrinksButton from './AddDrinksButton';
 const Card = props => {
     const drinkCount = props.drinkIds.length;
     const currentDrinkIndex = props.drinkIds.indexOf(props.id);
-    const stylePrevious = {
-        pointerEvents: currentDrinkIndex === 0 ? 'none' : null
-    }
-    const styleNext = {
-        pointerEvents: currentDrinkIndex + 1 === drinkCount ? 'none' : null
-
-    }
+    const previousDisabled = currentDrinkIndex === 0 ? true : false;
+    const nextDisabled = currentDrinkIndex + 1 === drinkCount ? true : false;
     const CardStyle = {
         backgroundImage: `url(${props.strDrinkThumb})`,
         backgroundSize: 'cover',
@@ -19,12 +14,12 @@ const Card = props => {
         backgroundPosition: 'center',
     }
     return (
-        <div className="card tc grow dib w-27" key={props.id} style={CardStyle} id={props.id} onClick={props.handleClick}>            
-            <h3 id={props.id} onClick={props.handleClick} className='bg-white-80 pa1'>{props.drinkName}</h3>            
+        <div className="card tc grow dib w-27" title={`A ${props.drinkName} served in a ${props.drinkGlass}`} style={CardStyle} id={props.id} onClick={props.handleClick}>             
+            <h3 id={props.id} className='bg-white-80 pa1'>{props.drinkName}</h3>            
             {props.isDrillDown ? 
             <div>
-                <button id={props.drinkIds[currentDrinkIndex - 1]} onClick={props.handleClick} style={stylePrevious}>Previous</button>
-                <button id={props.drinkIds[currentDrinkIndex + 1]} onClick={props.handleClick} style={styleNext}>Next</button>
+                <button id={props.drinkIds[currentDrinkIndex - 1]} onClick={props.handleClick} disabled={previousDisabled}>Previous</button>
+                <button id={props.drinkIds[currentDrinkIndex + 1]} onClick={props.handleClick} disabled={nextDisabled}>Next</button>
             </div> : null}
             <AddDrinksButton />           
         </div>
